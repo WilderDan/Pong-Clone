3b71223789e899c9fdb101f1362789bf715f9e8e
Dan Wilder <dwilder460@gmail.com>
2021-11-04 19:55:02 -0500

    Add title screen

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -5,3 +5,7 @@ signal start_game
 func _on_Start_pressed():
 	$Menu.hide()
 	emit_signal("start_game")
+
+
+func _on_Main_Menu_Button_pressed():
+	get_tree().change_scene("res://sceens/Main.tscn")
---
+++ scripts/Main.gd
@@ -0,0 +1,4 @@
+extends CanvasLayer
+
+func _on_PracticeButton_pressed():
+	get_tree().change_scene("res://sceens/Game.tscn")
